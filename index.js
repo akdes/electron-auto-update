f5849090f8f8a725aa67e463a5db857c76927f0a
SergeyKhmylov <sergey@khmylov.me>
2018-01-10 17:12:32 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let window = null;
+
+app.once('ready', function() {
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 500px
+    width: 500,
+    // Set the initial height to 400px
+    height: 400,
+    // set the background color to black
+    backgroundColor: "#111",
+    // Don't show the window until it's ready, this prevents any white flickering
+    show: false
+  })
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'static', 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  window.once('ready-to-show', function() {
+    window.show()
+  })
+})
